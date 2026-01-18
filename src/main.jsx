3f14a4ec00f5212b3dbb40eac721e5fb8f1cdcbe
sanka <sankamidhilesh@gmail.com>
2026-01-19 00:24:03 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./App.css";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./App.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
