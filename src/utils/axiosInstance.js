1a7b33e008192c763ef718081c5e48f017df0911
sanka <sankamidhilesh@gmail.com>
2026-01-18 23:35:01 +0530

    updated

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,10 +1,12 @@
 import axios from "axios"; // it is a js library used to make HTTP requests(GET,PUT,PATCH,POST,DELETE) from the browser or nodejs using promises
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://lap-tracker-backend-1.onrender.com",
   withCredentials: true,
 });
 
 export default api;
+
 
 
